a6f753ffa3023f83e35bdf7da7227030128575e1
cayoshi <nateeronron@gmail.com>
2025-01-15 09:59:56 +0700

    sadasd

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def docker_menu():
     default_image_name = "bot-run-test-v1"
     default_container_name = "bot-run-test-v1-container"
     port = "80:45441"
-    #dfdsasdasdasdasd
+    #dfdsaasd
     while True:
         print("\nDocker Management Menu")
         print("0. Auto ReBuild")
